8acd8962122a4cd9a053c54f7b94523b6e410f46
Trupti738 <p.trupti738@gmail.com>
2023-03-20 17:44:10 +0000

    first commit

---
+++ src/MySelf.java
@@ -0,0 +1,18 @@
+public class MySelf {
+    static int a =10;
+    static int b =40;
+static int c=20;
+static String name= "we are doing study";
+static String name1="Hello";
+static String place="London";
+    public static void main(String[] args) {
+
+       // System.out.println (b+=c);//60=b
+       // System.out.println(c-=a);//c=10
+       // System.out.println(b%=c);//b=6
+       // System.out.println("we are doing s\'tudy");
+
+
+
+    }
+}
